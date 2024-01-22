8e2107db942485b88f7cf0f93e9d369f9b1a2a34
Hugo-Hattori <136493140+Hugo-Hattori@users.noreply.github.com>
2024-01-22 19:35:14 -0300

    Converting processed image to black and white

--- teste_metodo.py
+++ teste_metodo.py
@@ -19,3 +19,19 @@ for metodo in metodos:
     i += 1
     _, imagem_tratada = cv2.threshold(imagem_cinza, 127, 255, metodo or cv2.THRESH_OTSU) #cv2.THRESH_OTSU é um método auxiliar
     cv2.imwrite(f'teste_metodo/imagemtratada_{i}.png' , imagem_tratada)
+
+
+# após rodar o código acima descobri que o método THRESH_TRUNC é o melhor para o tratamento inicial
+imagem = Image.open('teste_metodo/imagemtratada_3.png')
+imagem = imagem.convert('P') #garantindo que a imagem estará em tons de cinza
+imagem2 = Image.new(mode='P', size=imagem.size, color=(255, 255, 255))
+
+# transformando a imagem em preto e branco puros
+for x in range(imagem.size[1]): #percorrendo a largura da imagem
+    for y in range(imagem.size[0]): #percorrendo a altura da imagem
+        cor_pixel = imagem.getpixel((y, x))
+        if cor_pixel < 115:
+            #pinta de preto, os demais permanecerão brancos
+            imagem2.putpixel((y, x), (0, 0, 0))
+
+imagem2.save('teste_metodo/imagem_final.png')
