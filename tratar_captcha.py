92a1efda16b8eeb7127adace09713e73b95229a2
Hugo-Hattori <136493140+Hugo-Hattori@users.noreply.github.com>
2024-01-23 10:28:06 -0300

    Applying black and white treatment to all images

--- tratar_captcha.py
+++ tratar_captcha.py
@@ -1,6 +1,7 @@
 import cv2
 import os
 import glob
+from PIL import Image
 
 
 def tratar_imagens(pasta_origem, pasta_destino='imgs_ajustadas'):
@@ -14,6 +15,23 @@ def tratar_imagens(pasta_origem, pasta_destino='imgs_ajustadas'):
         _, imagem_tratada = cv2.threshold(imagem_cinza, 127, 255, cv2.THRESH_TRUNC or cv2.THRESH_OTSU)
         nome_arquivo = os.path.basename(arquivo)
         cv2.imwrite(f'{pasta_destino}/{nome_arquivo}', imagem_tratada)
+
+    arquivos = glob.glob(f'{pasta_destino}/*')
+    for arquivo in arquivos:
+        imagem = Image.open(arquivo)
+        imagem = imagem.convert('P')  # garantindo que a imagem estará em tons de cinza
+        imagem2 = Image.new(mode='P', size=imagem.size, color=(255, 255, 255))
+
+        # aplicando tratamento 2
+        for x in range(imagem.size[1]):  # percorrendo a largura da imagem
+            for y in range(imagem.size[0]):  # percorrendo a altura da imagem
+                cor_pixel = imagem.getpixel((y, x))
+                if cor_pixel < 115:
+                    # pinta de preto, os demais permanecerão brancos
+                    imagem2.putpixel((y, x), (0, 0, 0))
+
+        nome_arquivo = os.path.basename(arquivo)
+        imagem2.save(f'{pasta_destino}/{nome_arquivo}')
 
 
 if __name__ == "__main__":
